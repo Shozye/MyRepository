4b6cd5b782ddf60c2716c601b7c56a921021ffed
Mateusz <sshozye@gmail.com>
2019-06-10 22:29:18 +0200

    Created test list and solution list to handle handtests better

--- Nice code/Clearcode Entry Test/handtests.py
+++ Nice code/Clearcode Entry Test/handtests.py
@@ -1,19 +1,26 @@
 from main import calculate
 
 def handtests():
-    print(calculate(0, []))
-    print(calculate( 0, [("Mickey Mouse", 0, 100), ("Mickey Donald", 1, 200)] ) )
-    #What happens if worth is negative?
-    print(calculate(0, [("Very Bad Meme :(", 0, -10), ("Good Meme :)", 0, 10)]))
-
-    # Now example test given in statement
-    usb_size = 1
-    memes = [
-      ('rollsafe.jpg', 205, 6),
-      ('sad_pepe_compilation.gif', 410, 10),
-      ('yodeling_kid.avi', 605, 12)
+    tests = [
+        calculate(0, []),
+        calculate( 0, [("Mickey Mouse", 0, 100), ("Mickey Donald", 1, 200)] ),
+        calculate(0, [("Very Bad Meme :(", 0, -10), ("Good Meme :)", 0, 10)]),
+        calculate(1, [('rollsafe.jpg', 205, 6),('sad_pepe_compilation.gif', 410, 10),('yodeling_kid.avi', 605, 12)] )
+        ]
+    solutions = [
+        (0, set([])),
+        (100, set(["Mickey Mouse"])),
+        (10, set(["Good Meme :)"])),
+        (22, set(["sad_pepe_compilation.gif", "yodeling_kid.avi"]))
     ]
-    print(calculate(usb_size, memes))
-
-    
+    passed = 0
+    not_passed = 0
+    for TestNum in range(len(tests)):
+        if tests[0] == solutions[0]:
+            print(f"Test {TestNum + 1} passed")
+            passed += 1
+        else:
+            print(f"Test {TestNum + 1} not passed")
+            not_passed += 1
+    print(f"Tests passed : {passed} \nTests not passed: {not_passed}")
 handtests()
