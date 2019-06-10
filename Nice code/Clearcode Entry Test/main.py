a311ca0c8daa98af1088092103dd0e7e3ef28f5a
Mateusz <sshozye@gmail.com>
2019-06-10 18:45:46 +0200

    Adding handtests as distinct file, not as function of main.py

--- Nice code/Clearcode Entry Test/main.py
+++ Nice code/Clearcode Entry Test/main.py
@@ -46,18 +46,3 @@ def calculate(usb_size, memes):
         # return Tuple(int, Set(string*))
     return (best_answer[VALUE], best_answer[NAMES]) 
 
-def handtests():
-    print(calculate(0, []))
-    print(calculate( 0, [("Mickey Mouse", 0, 100), ("Mickey Donald", 1, 200)] ) )
-    #What happens if worth is negative?
-    print(calculate(0, [("Very Bad Meme :(", 0, -10), ("Good Meme :)", 0, 10)]))
-
-    # Now example test given in statement
-    usb_size = 1
-    memes = [
-      ('rollsafe.jpg', 205, 6),
-      ('sad_pepe_compilation.gif', 410, 10),
-      ('yodeling_kid.avi', 605, 12)
-    ]
-    print(calculate(usb_size, memes))
-#handtests()
